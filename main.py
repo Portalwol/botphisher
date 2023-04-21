22d44f5c7599a4dd3fb38e74c906e56c30ba760f
Portalwol <yafidafi@gmail.com>
2023-04-20 17:16:58 -0700

    auto setup server command

--- main.py
+++ main.py
@@ -270,6 +270,86 @@ async def on_ready():
 async def setup(ctx):
     await ctx.send(embed=embed, view=MyView(timeout=None))
 
+@client.slash_command(name="setupserver", description="tshi will delete all chanenl in servaer.")
+async def setup(ctx):
+    for channel in ctx.guild.channels:
+        author = ctx.author
+        await channel.delete()
+
+    # create all categories 
+    Verification = await ctx.guild.create_category("✅ | Verfication")
+    Important = await ctx.guild.create_category("📢 | Important")
+    Carries = await ctx.guild.create_category("🛡️| Carries")
+    Support = await ctx.guild.create_category("📙 | Support")
+    
+    # change all category permissions
+    await Verification.set_permissions(ctx.guild.default_role, send_messages=False, add_reactions=False)
+    await Important.set_permissions(ctx.guild.default_role, send_messages=False, add_reactions=False)
+    await Carries.set_permissions(ctx.guild.default_role, send_messages=False, add_reactions=False)
+    await Support.set_permissions(ctx.guild.default_role, send_messages=False, add_reactions=False)
+
+    # create all channels
+    Verify = await ctx.guild.create_text_channel("✅│verify", category=Verification)
+    Rules = await ctx.guild.create_text_channel("📜│rules", category=Important)
+    Announcements = await ctx.guild.create_text_channel("📢│announcements", category=Important)
+    Carry = await ctx.guild.create_text_channel("⚔️│carries", category=Carries)
+    Support = await ctx.guild.create_text_channel("📙│support", category=Support)
+    # config certain channels
+    await Announcements.set_permissions(ctx.guild.default_role, send_messages=False, add_reactions=False, read_messages=False)
+
+    #create roles
+    guild = ctx.guild
+    permissions = discord.Permissions(send_messages=True, read_messages=True)
+    adminperms = discord.Permissions(administrator=True)
+    await guild.create_role(name="Carrier", permissions=adminperms, color=0x90EE90)
+    await guild.create_role(name="Guild Member", permissions=permissions, color=0x0000)
+    await guild.create_role(name="Top Tier Member", permissions=permissions, color=0xa020f0)
+    await guild.create_role(name="Staff", permissions=adminperms, color=0xFFFF00)
+    await guild.create_role(name="Admin", permissions=adminperms, color=0xFF0000)
+
+    # make rules embed
+    rulesembed = discord.Embed(
+        title = ctx.guild.name.title() + " Rules",
+        description = "Please read the rules below before continuing.",
+        color = 0x90EE90)
+    rulesembed.add_field(name="1.) No spamming.", value="Spamming will get you muted.", inline=False)
+    rulesembed.add_field(name="2.) No NSFW content.", value="NSFW content is not tolerated and will get you instantly temp-banned.", inline=False)
+    rulesembed.add_field(name="3.) No racism.", value="Racism is not tolerated and will get you instantly and permanently banned", inline=False)
+    rulesembed.add_field(name="4.) No advertising.", value="Advertising will get you muted.", inline=False)
+    rulesembed.add_field(name="5.) No impersonation.", value="Impersonating anybody, even if it's a regular member will get you banned.", inline=False)
+    rulesembed.add_field(name="6.) No bullying.", value="Bullying members is not allowed and will get you muted or banned if it's continued in dms.", inline=False)
+    rulesembed.add_field(name="7.) No self-promotion.", value="Self promoting will get you muted.", inline=False)
+    rulesembed.add_field(name="8.) No asking for staff.", value="Asking for staff beyond jokes will get you muted.", inline=False)
+    await Rules.send(embed=rulesembed)
+
+    # make carries embed
+    carriesembed = discord.Embed(
+        title = "☠️ Catacombs Carries",
+        description="All specifics for dungeon carries.",
+        color = 0x90EE90
+    )
+    carriesembed.add_field(name="Floor 1", value="Price: 50k", inline=False)
+    carriesembed.add_field(name="Floor 2", value="Price: 100k", inline=False)
+    carriesembed.add_field(name="Floor 3", value="Price: 150k", inline=False)
+    carriesembed.add_field(name="Floor 4", value="Price: 200k", inline=False)
+    carriesembed.add_field(name="Floor 5", value="Price 300k", inline=False)
+    carriesembed.add_field(name="Floor 6", value="Price: 600k", inline=False)
+    carriesembed.add_field(name="Floor 7", value="Price: 3m", inline=False)
+    channel = discord.utils.get(client.get_all_channels(), name='📙│support')
+    if channel:
+        channel_id = channel.id
+        carriesembed.add_field(name="Create a ticket", value=f"at <#{channel_id}> to get a carry!", inline=False)
+    else:
+        print('Channel not found!')
+        carriesembed.add_field(name="Create a ticket", value=f"at #Support to get a carry!", inline=False)
+    await Carry.send(embed=carriesembed)
+    carriesfree = discord.Embed(
+        title = "☠️ Everyone who verified gets a free carry.",
+        description=f"As to grow the server we have made it so anybody who joined and verified get a free carry of the floor of their choice. Make a ticket in <#{channel_id}>.",
+        color = 0x90EE90
+    )
+    await Carry.send(embed=carriesfree)
+    await author.send(f"Please add ticket <https://ticketsbot.net/> bot and add it to the <#{channel_id}> section!")
 
 
 
